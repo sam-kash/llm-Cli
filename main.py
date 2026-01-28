d09350b4123f9ba7d1f7d5d172ed50af752fca19
sam-kash <kashyapsankalpa@gmail.com>
2026-01-28 23:50:36 +0530

    llama talking now

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-import os 
-from openai import OpenAI
+import os
 from dotenv import load_dotenv
+from groq import Groq
 
 load_dotenv()
 
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
-response = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
-        {"role":"user", "content":"Say hellow like pirate"}
-    ]
-)
+print(" Talk to the LLM (type 'exit' to quit)\n")
 
-print(response.choices[0].message.content)
+while True:
+    user_input = input("You: ")
+
+    if user_input.lower() == "exit":
+        print("Goodbye")
+        break
+
+    response = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[
+            {"role": "user", "content": user_input}
+        ],
+        temperature=1.9,
+    )
+
+    print("\nLLM:", response.choices[0].message.content, "\n")
